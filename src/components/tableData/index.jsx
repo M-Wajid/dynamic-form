1351179e3b619ffb8ed0bbd7e2c1ded406b472e8
M-Wajid <wajidaman5@gmail.com>
2023-02-25 02:09:21 +0500

    without edit

--- src/components/tableData/index.jsx
+++ src/components/tableData/index.jsx
@@ -1,28 +1,55 @@
-import React from 'react'
+import React from "react";
 
 const TableData = (props) => {
-  const {users, setUsersData, singleUser, setSingleUserData} = props;
+  const { users, setUsersData, singleUser, setSingleUserData } = props;
 
-  const onChangeHandler = (event)=>{
-    setSingleUserData({...singleUser, [event.target.name]:event.target.value})
-  }
+  const onChangeHandler = (event) => {
+    setSingleUserData({
+      ...singleUser,
+      [event.target.name]: event.target.value,
+    });
+  };
 
   const onClickHandler = (event) => {
     let array = [...users];
     array.push(singleUser);
     setUsersData(array);
-    console.log(users)
-  }
+    console.log(users);
+  };
 
   return (
     <div>
-      <input name='name' type='text' placeholder='Name' onChange={onChangeHandler} />
-      <input name='email' type='text' placeholder='Email' onChange={onChangeHandler} />
-      <input name='gender' type='text' placeholder='Gender' onChange={onChangeHandler} />
-      <input name='ocupation' type='text' placeholder='Ocupation' onChange={onChangeHandler} />
+      <input
+        name="name"
+        type="text"
+        placeholder="Name"
+        autoComplete="off"
+        onChange={onChangeHandler}
+      />
+      <input
+        name="email"
+        type="text"
+        placeholder="Email"
+        autoComplete="off"
+        onChange={onChangeHandler}
+      />
+      <input
+        name="gender"
+        type="text"
+        placeholder="Gender"
+        autoComplete="off"
+        onChange={onChangeHandler}
+      />
+      <input
+        name="ocupation"
+        type="text"
+        placeholder="Ocupation"
+        autoComplete="off"
+        onChange={onChangeHandler}
+      />
       <button onClick={onClickHandler}>submit</button>
     </div>
-  )
-}
+  );
+};
 
 export default TableData;
--- src/components/tableView/index.jsx
+++ src/components/tableView/index.jsx
@@ -1,35 +1,51 @@
-import React from 'react'
+import React from "react";
 
 const TableView = (props) => {
-  const {users, setUsersData, singleUser, setSingleUserData} = props;
-  
+  const { users, setUsersData, singleUser, setSingleUserData } = props;
+
+  const deleteFunc = (index) => {
+    let array = [...users];
+    array.splice(index, 1);
+    setUsersData(array);
+  };
+
+  const editFunc = (index) => {
+    let array = [...users];
+
+  };
+
   return (
     <div>
-        <table>
-            <tbody>
-                <tr>
-                    <td>Name</td>
-                    <td>Email</td>
-                    <td>Gender</td>
-                    <td>Ocupation</td>
-                </tr>
-                {
-                    users && users.map((item,index)=>{
-                        return(
-                          <tr>
-                            <td>{item.name}</td>
-                            <td>{item.email}</td>
-                            <td>{item.gender}</td>
-                            <td>{item.ocupation}</td>
-                          </tr>  
-                        )
-                        
-                    })
-                }
-            </tbody>
-        </table>
+      <table border="1" width="100%">
+        <tbody>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Gender</td>
+            <td>Ocupation</td>
+            <td>Edit Data</td>
+            <td>Delete Data</td>
+          </tr>
+          {users.map((item, index) => {
+            return (
+              <tr>
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+                <td>{item.gender}</td>
+                <td>{item.ocupation}</td>
+                <td>
+                  <button onClick={()=>editFunc(index)}>Edit</button>
+                </td>
+                <td>
+                  <button onClick={() => deleteFunc(index)}>Delete</button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
 export default TableView;
